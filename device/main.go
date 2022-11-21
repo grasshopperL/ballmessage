aebbfbfc62e9d25e71ebdaae28c69dd0223790d2
U-360BUYAD\liubaoshuai3 <liubaoshuai3@ZB-PF0YQNL2.360buyAD.local>
2022-11-21 18:46:07 +0800

    add get/set config and find/kill process function

--- device/main.go
+++ device/main.go
@@ -1,11 +1,67 @@
 package main
 
 import (
+	"github.com/shirou/gopsutil/v3/process"
+	"github.com/spf13/viper"
 	"log"
 	"os"
 	"os/signal"
+	"path"
+	"sync"
 	"syscall"
 )
+
+// getConfig get config value from config file according key
+func getConfig(key string) (bool, interface{}) {
+
+	defer configLock.Unlock()
+	configLock.Lock()
+	return viper.IsSet(key), viper.Get(key)
+}
+
+// setConfig write config value to config file according key
+func setConfig(key string, value interface{}) error {
+
+	defer configLock.Unlock()
+	configLock.Lock()
+	viper.Set(key, value)
+	return viper.WriteConfigAs(path.Join(configDir, configName))
+}
+
+// findProcess finds pid according to process name
+func findProcess(name string) int {
+
+	processes, err := process.Processes()
+	if err != nil {
+		return -1
+	}
+	for _, p := range processes {
+		if n, err := p.Name(); err == nil && n == name {
+			return int(p.Pid)
+		}
+	}
+	return -1
+}
+
+// killProcess kill process according to process name
+func killProcess(name string) error {
+
+	processes, err := process.Processes()
+	if err != nil {
+		return err
+	}
+	for _, p := range processes {
+		if n, err := p.Name(); err == nil && n == name {
+			return p.Kill()
+		}
+	}
+	return nil
+}
+
+// configLock is mutex lock for config file
+var configLock sync.Mutex
+var configDir string
+var configName string
 
 func main() {
 
---
+++ device/main_test.go
@@ -0,0 +1,7 @@
+package main
+
+import "testing"
+
+func TestGetConfig(t *testing.T) {
+	getConfig("a")
+}
